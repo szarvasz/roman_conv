7c2544fe1e9ec7c4c693a93521e4d3268dc9f3f6
Szarvas Zoltán <zoltan.szarvas@idomsoft.hu>
2020-09-28 21:41:41 +0200

    first iteration: 1 --> I

---
+++ src/main/java/hu/szarvasz/romanconv/Converter.java
@@ -0,0 +1,7 @@
+package hu.szarvasz.romanconv;
+
+public class Converter {
+    public static String convert(int i) {
+        return "I";
+    }
+}
--- src/test/java/hu/szarvasz/romanconv/ConverterTest.java
+++ src/test/java/hu/szarvasz/romanconv/ConverterTest.java
@@ -2,12 +2,13 @@ package hu.szarvasz.romanconv;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ConverterTest {
 
     @Test
-    public void test(){
-        assertTrue(true);
+    public void testI(){
+        assertEquals("I", Converter.convert(1));
     }
 }
