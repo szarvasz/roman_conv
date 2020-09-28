35b8b059a6ceb4bb85758a6d0ed4c3586feced99
Szarvas Zoltán <zoltan.szarvas@idomsoft.hu>
2020-09-28 21:35:40 +0200

    init

---
+++ src/test/java/hu/szarvasz/romanconv/ConverterTest.java
@@ -0,0 +1,13 @@
+package hu.szarvasz.romanconv;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ConverterTest {
+
+    @Test
+    public void test(){
+        assertTrue(true);
+    }
+}
