690d51a938a41b249303ee058f8ef37f377596a4
Szarvas Zoltán <zoltan.szarvas@idomsoft.hu>
2020-09-28 21:51:10 +0200

    next iteration: 4 --> IV

--- src/main/java/hu/szarvasz/romanconv/Converter.java
+++ src/main/java/hu/szarvasz/romanconv/Converter.java
@@ -4,8 +4,12 @@ public class Converter {
 
     public static String convert(int arabNum) {
         String romanNum = "";
-        for (int i = 0; i < arabNum; i++) {
-            romanNum += "I";
+        if (arabNum == 4){
+            romanNum = "IV";
+        } else {
+            for (int i = 0; i < arabNum; i++) {
+                romanNum += "I";
+            }
         }
         return romanNum;
     }
--- src/test/java/hu/szarvasz/romanconv/ConverterTest.java
+++ src/test/java/hu/szarvasz/romanconv/ConverterTest.java
@@ -21,4 +21,9 @@ public class ConverterTest {
     public void test_III(){
         assertEquals("III", Converter.convert(3));
     }
+
+    @Test
+    public void test_IV(){
+        assertEquals("IV", Converter.convert(4));
+    }
 }
